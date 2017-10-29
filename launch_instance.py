6f88911cdc38007fccec22c5a4f35b3268b5115d
KoutaCS <chits.boorana@hotmail.com>
2017-10-29 12:12:35 +0700

    fix error

--- launch_instance.py
+++ launch_instance.py
@@ -3,7 +3,7 @@ import jsonschema
 from rally.common import logging
 from rally import consts
 from rally.plugins.openstack import scenario 
-from rally.plugins.openstack.nova import utils as nova_utils
+from rally.plugins.openstack.scenarios import utils as nova_utils
 from rally.plugins.openstack.wrappers import network as network_wrapper
 from rally.task import types
 from rally.task import validation
