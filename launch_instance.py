ba22d0e965e7dce312543bb76ba59f14e752c040
KoutaCS <chits.boorana@hotmail.com>
2017-10-30 15:28:10 +0700

    fix error

--- launch_instance.py
+++ launch_instance.py
@@ -2,9 +2,10 @@ import jsonschema
 
 from rally.common import logging
 from rally import consts
-from rally.plugins.openstack import scenario 
+from rally.plugins.openstack import scenario
 from rally.plugins.openstack.scenarios.nova import utils as nova_utils
 from rally.plugins.openstack.wrappers import network as network_wrapper
+from rally.plugins.openstack.scenarios.neutron import utils as neutron_utils
 from rally.task import types
 from rally.task import validation
 
@@ -18,17 +19,16 @@ from rally.task import validation
 
 @types.convert(image={"type": "glance_image"}, flavor={"type": "nova_flavor"})
 @validation.add("image_valid_on_flavor", flavor_param="flavor", image_param="image")
-@validation.add("required_services", services=(consts.Service.NOVA))
+@validation.add("required_services", services=[consts.Service.NOVA, consts.Service.NEUTRON])
 @validation.add("required_platform", platform="openstack", users=True)
 @validation.add("required_contexts", contexts=("network"))
-@scenario.configure(context={"cleanup@openstack": ["nova", "neutron.floatingip"]}, name="NipaCloud.launch_single_instance", platform="openstack")
+@scenario.configure(context={"cleanup@openstack": ["nova", "neutron"]}, name="NipaCloud.launch_single_instance", platform="openstack")
 
-class LaunchSingleInstance(nova_utils.NovaScenario):
+class LaunchSingleInstance(nova_utils.NovaScenario, neutron_utils.NeutronScenario):
     def run(self, image, flavor, force_delete=False):
         keypair = self._create_keypair()
-        server = self._boot_server(image, flavor, key_name=keypair)
         security_group = self._create_security_group()
-        self._add_server_secgroups(server, security_group)
+        server = self._boot_server(image, flavor, key_name=keypair, secgroups=security_group)
         address = network_wrapper.wrap(self.clients, self).create_floating_ip(
             tenant_id=server.tenant_id)
         self._associate_floating_ip(server, address["ip"])
